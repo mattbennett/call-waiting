ebf166df751f8ddd1a0b1d9dccd699506372ea12
Matt Bennett <matt@bennett.name>
2016-05-26 13:54:35 +0100

    new implementation supporting extra args in callback and yielding the result

--- call_waiting.py
+++ call_waiting.py
@@ -1,28 +1,59 @@
+import sys
 from contextlib import contextmanager
 from threading import Semaphore
 
-
-def maybe_release(sem, callback, args, kwargs):
-    should_release = True
-    if callable(callback):
-        should_release = callback(*args, **kwargs)
-
-    if should_release:
-        sem.release()
+from mock import patch
 
 
 @contextmanager
 def patch_wait(obj, target, callback=None):
-    from mock import patch
+
+    class WaitResult(object):
+        res = None
+        exc_info = None
+
+        @property
+        def has_result(self):
+            return self.res is not None or self.exc_info is not None
+
+        def send(self, res, exc_info):
+            if not self.has_result:
+                self.res = res
+                self.exc_info = exc_info
+
+        def get(self):
+            if self.exc_info is not None:
+                raise self.exc_info[1]
+            return self.res
 
     sem = Semaphore(0)
+    result = WaitResult()
+
     unpatched = getattr(obj, target)
 
+    def maybe_release(args, kwargs, res, exc_info):
+        should_release = True
+        if callable(callback):
+            should_release = callback(args, kwargs, res, exc_info)
+
+        if should_release:
+            result.send(res, exc_info)
+            sem.release()
+
     def wraps(*args, **kwargs):
-        res = unpatched(*args, **kwargs)
-        maybe_release(sem, callback, args, kwargs)
+        res = None
+        exc_info = None
+        try:
+            res = unpatched(*args, **kwargs)
+        except Exception:
+            exc_info = sys.exc_info()
+
+        maybe_release(args, kwargs, res, exc_info)
+
+        if exc_info is not None:
+            raise exc_info[1]
         return res
 
-    with patch.object(obj, target, wraps=wraps):
-        yield
+    with patch.object(obj, target, new=wraps):
+        yield result
         sem.acquire()
